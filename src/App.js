4f069583b8141109f25e6d8745256b415792be96
TanzirulShanto <tanzirulislam025@gmail.com>
2024-02-23 15:54:44 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
+
 
 function App() {
+  const [role, setRole] = useState('developer');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {showEmployees ? (
+          <>
+            <input type="tetx" onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}/>
+
+            <Employee name="Jalal Udiin" role="Teacher and Researcher" />
+            <Employee name="Tanzirul Islam" role={role}/>
+            
+          </>
+        ) : (
+          <p>You don't have the access to the Employee</p>
+
+        )}
+
     </div>
   );
 }
